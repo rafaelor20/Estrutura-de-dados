99c63328e5ebd635d98c25229fddc0ac4acbd207
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-07-30 20:59:15 -0300

    Segundo exercicio, TestaPonteiros, 30-07

---
+++ exercicio_qual_quadrante/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rafael/Documentos/Projetos/UFS/Estrutura-de-dados/exercicio_qual_quadrante
+# Build directory: /home/rafael/Documentos/Projetos/UFS/Estrutura-de-dados/exercicio_qual_quadrante/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- exercicio_qual_quadrante/quadrante.c
+++ exercicio_qual_quadrante/quadrante.c
@@ -8,18 +8,18 @@ int main() {
     scanf("%i",&y);
     if (x < 0){
         if (y < 0){
-            printf("O ponto está no 3º quadrante.\n");
+            printf("O ponto esta no 3º quadrante.\n");
         }
         else{
-            printf("O ponto está no 2º quadrante.\n");
+            printf("O ponto esta no 2º quadrante.\n");
         }
     }
     else{
         if (y < 0){
-            printf("O ponto está no 4º quadrante.\n");
+            printf("O ponto esta no 4º quadrante.\n");
         }
         else{
-            printf("O ponto está no 1º quadrante.\n");
+            printf("O ponto esta no 1º quadrante.\n");
             }
     }
     return 0;
